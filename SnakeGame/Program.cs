2b8cb6827b87e9a1336aafda94b3780106cd531e
Jakub Bunar <jbunar200318@gmail.com>
2026-01-28 20:54:52 +0100

    Feature: added multiplayer

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -10,23 +10,26 @@ class Program
     {
         Console.WindowHeight = 18;
         Console.WindowWidth = 32;
-
         int screenwidth = Console.WindowWidth;
         int screenheight = 16;
-
         Random randomnummer = new Random();
 
-        Pixel hoofd = new Pixel();
-        hoofd.xPos = screenwidth / 2;
-        hoofd.yPos = screenheight / 2;
-        hoofd.schermKleur = ConsoleColor.Red;
+        Pixel waz1 = new Pixel();
+        waz1.xPos = screenwidth / 2;
+        waz1.yPos = screenheight / 2;
+        waz1.schermKleur = ConsoleColor.Red;
+        string movement1 = "RIGHT";
+        List<int> ogon1 = new List<int>();
+        int score1 = 0;
 
-        string movement = "RIGHT";
-        int score = 0;
+        Pixel waz2 = new Pixel();
+        waz2.xPos = screenwidth / 3;
+        waz2.yPos = screenheight / 3;
+        waz2.schermKleur = ConsoleColor.Yellow;
+        string movement2 = "D";
+        List<int> ogon2 = new List<int>();
+        int score2 = 0;
 
-        List<int> telje = new List<int>();
-
-        DateTime tijd = DateTime.Now;
         string obstacle = "*";
         int obstacleXpos = randomnummer.Next(1, screenwidth - 1);
         int obstacleYpos = randomnummer.Next(1, screenheight - 1);
@@ -42,44 +45,37 @@ class Program
             Console.ForegroundColor = ConsoleColor.White;
             for (int i = 0; i < screenwidth; i++)
             {
-                Console.SetCursorPosition(i, 0);
-                Console.Write("■");
-                Console.SetCursorPosition(i, screenheight - 1);
-                Console.Write("■");
+                Console.SetCursorPosition(i, 0); Console.Write("■");
+                Console.SetCursorPosition(i, screenheight - 1); Console.Write("■");
             }
             for (int i = 0; i < screenheight; i++)
             {
-                Console.SetCursorPosition(0, i);
-                Console.Write("■");
-                Console.SetCursorPosition(screenwidth - 1, i);
-                Console.Write("■");
+                Console.SetCursorPosition(0, i); Console.Write("■");
+                Console.SetCursorPosition(screenwidth - 1, i); Console.Write("■");
             }
 
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.SetCursorPosition(2, screenheight);
-            Console.WriteLine("Score: " + score);
+            Console.SetCursorPosition(1, screenheight);
+            Console.WriteLine($"P1: {score1} | P2: {score2}");
 
             Console.ForegroundColor = ConsoleColor.Green;
-            for (int i = 0; i < telje.Count; i += 2)
+            for (int i = 0; i < ogon1.Count; i += 2)
             {
-                Console.SetCursorPosition(telje[i], telje[i + 1]);
+                Console.SetCursorPosition(ogon1[i], ogon1[i + 1]);
                 Console.Write("■");
             }
-
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.SetCursorPosition(hoofd.xPos, hoofd.yPos);
+            Console.SetCursorPosition(waz1.xPos, waz1.yPos);
             Console.Write("■");
 
-            Console.SetCursorPosition(hoofd.xPos, hoofd.yPos);
-
-            Console.Write("■");
-
-            Console.SetCursorPosition(hoofd.xPos, hoofd.yPos);
-
-            Console.Write("■");
-
-            Console.SetCursorPosition(hoofd.xPos, hoofd.yPos);
-
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            for (int i = 0; i < ogon2.Count; i += 2)
+            {
+                Console.SetCursorPosition(ogon2[i], ogon2[i + 1]);
+                Console.Write("■");
+            }
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.SetCursorPosition(waz2.xPos, waz2.yPos);
             Console.Write("■");
 
             if (Console.KeyAvailable)
@@ -87,84 +83,78 @@ class Program
                 ConsoleKeyInfo info = Console.ReadKey(true);
                 switch (info.Key)
                 {
-                    case ConsoleKey.UpArrow:
-                        if (movement != "DOWN") movement = "UP";
-                        break;
-                    case ConsoleKey.DownArrow:
-                        if (movement != "UP") movement = "DOWN";
-                        break;
-                    case ConsoleKey.LeftArrow:
-                        if (movement != "RIGHT") movement = "LEFT";
-                        break;
-                    case ConsoleKey.RightArrow:
-                        if (movement != "LEFT") movement = "RIGHT";
-                        break;
+                    case ConsoleKey.UpArrow: if (movement1 != "DOWN") movement1 = "UP"; break;
+                    case ConsoleKey.DownArrow: if (movement1 != "UP") movement1 = "DOWN"; break;
+                    case ConsoleKey.LeftArrow: if (movement1 != "RIGHT") movement1 = "LEFT"; break;
+                    case ConsoleKey.RightArrow: if (movement1 != "LEFT") movement1 = "RIGHT"; break;
+
+                    case ConsoleKey.W: if (movement2 != "S") movement2 = "W"; break;
+                    case ConsoleKey.S: if (movement2 != "W") movement2 = "S"; break;
+                    case ConsoleKey.A: if (movement2 != "D") movement2 = "A"; break;
+                    case ConsoleKey.D: if (movement2 != "A") movement2 = "D"; break;
                 }
             }
 
-            telje.Insert(0, hoofd.xPos);
-            telje.Insert(1, hoofd.yPos);
+            ogon1.Insert(0, waz1.xPos);
+            ogon1.Insert(1, waz1.yPos);
+            if (movement1 == "UP") waz1.yPos--;
+            if (movement1 == "DOWN") waz1.yPos++;
+            if (movement1 == "LEFT") waz1.xPos--;
+            if (movement1 == "RIGHT") waz1.xPos++;
 
-            if (movement == "UP") hoofd.yPos--;
-            if (movement == "DOWN") hoofd.yPos++;
-            if (movement == "LEFT") hoofd.xPos--;
-            if (movement == "RIGHT") hoofd.xPos++;
+            ogon2.Insert(0, waz2.xPos);
+            ogon2.Insert(1, waz2.yPos);
+            if (movement2 == "W") waz2.yPos--;
+            if (movement2 == "S") waz2.yPos++;
+            if (movement2 == "A") waz2.xPos--;
+            if (movement2 == "D") waz2.xPos++;
 
-            if (hoofd.xPos == obstacleXpos && hoofd.yPos == obstacleYpos)
+            if (waz1.xPos == obstacleXpos && waz1.yPos == obstacleYpos)
             {
-                score++;
+                score1++;
                 obstacleXpos = randomnummer.Next(1, screenwidth - 1);
                 obstacleYpos = randomnummer.Next(1, screenheight - 1);
             }
-            else
-            {
-                if (telje.Count > 0)
-                {
-                    telje.RemoveAt(telje.Count - 1);
-                    telje.RemoveAt(telje.Count - 1);
-                }
-            }
+            else if (ogon1.Count > 0) { ogon1.RemoveAt(ogon1.Count - 1); ogon1.RemoveAt(ogon1.Count - 1); }
 
-            if (hoofd.xPos <= 0  hoofd.xPos >= screenwidth - 1  hoofd.yPos <= 0 || hoofd.yPos >= screenheight - 1)
+            if (waz2.xPos == obstacleXpos && waz2.yPos == obstacleYpos)
             {
-                Console.Clear();
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.SetCursorPosition(screenwidth / 5, screenheight / 2);
-                Console.WriteLine("Game Over");
-                Console.SetCursorPosition(screenwidth / 5, screenheight / 2 + 1);
-                Console.WriteLine("Twój wynik: " + score);
-                Console.SetCursorPosition(screenwidth / 5, screenheight / 2 + 2);
-                Environment.Exit(0);
+                score2++;
+                obstacleXpos = randomnummer.Next(1, screenwidth - 1);
+                obstacleYpos = randomnummer.Next(1, screenheight - 1);
             }
+            else if (ogon2.Count > 0) { ogon2.RemoveAt(ogon2.Count - 1); ogon2.RemoveAt(ogon2.Count - 1); }
 
-            for (int i = 0; i < telje.Count; i += 2)
-            {
-                if (hoofd.xPos == telje[i] && hoofd.yPos == telje[i + 1])
-                {
-                    Console.Clear();
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.SetCursorPosition(screenwidth / 5, screenheight / 2);
-                    Console.WriteLine("Game Over");
-                    Console.SetCursorPosition(screenwidth / 5, screenheight / 2 + 1);
-                    Console.WriteLine("Twój wynik: " + score);
-                    Console.SetCursorPosition(screenwidth / 5, screenheight / 2 + 2);
-                    Environment.Exit(0);
-                }
-            }
+            if (waz1.xPos <= 0 || waz1.xPos >= screenwidth - 1 || waz1.yPos <= 0 || waz1.yPos >= screenheight - 1)
+                GameOver("Gracz 1 uderzył w ścianę! Wygrał Gracz 2.");
+            if (waz2.xPos <= 0 || waz2.xPos >= screenwidth - 1 || waz2.yPos <= 0 || waz2.yPos >= screenheight - 1)
+                GameOver("Gracz 2 uderzył w ścianę! Wygrał Gracz 1.");
+
+            for (int i = 0; i < ogon1.Count; i += 2)
+                if (waz1.xPos == ogon1[i] && waz1.yPos == ogon1[i + 1]) GameOver("Gracz 1 zjadł ogon! Wygrał Gracz 2.");
+            for (int i = 0; i < ogon2.Count; i += 2)
+                if (waz2.xPos == ogon2[i] && waz2.yPos == ogon2[i + 1]) GameOver("Gracz 2 zjadł ogon! Wygrał Gracz 1.");
+
+            if (waz1.xPos == waz2.xPos && waz1.yPos == waz2.yPos) GameOver("Remis! Zderzenie czołowe.");
+
+            for (int i = 0; i < ogon2.Count; i += 2)
+                if (waz1.xPos == ogon2[i] && waz1.yPos == ogon2[i + 1]) GameOver("Gracz 1 wjechał w Gracza 2! Wygrał Gracz 2.");
+
+            for (int i = 0; i < ogon1.Count; i += 2)
+                if (waz2.xPos == ogon1[i] && waz2.yPos == ogon1[i + 1]) GameOver("Gracz 2 wjechał w Gracza 1! Wygrał Gracz 1.");
 
             Thread.Sleep(100);
         }
     }
 
-    static void GameOver(int score, int width, int height)
+    static void GameOver(string info)
     {
         Console.Clear();
         Console.ForegroundColor = ConsoleColor.Red;
-        Console.SetCursorPosition(width / 5, height / 2);
-        Console.WriteLine("Game Over");
-        Console.SetCursorPosition(width / 5, height / 2 + 1);
-        Console.WriteLine("Twój wynik: " + score);
-        Console.SetCursorPosition(width / 5, height / 2 + 2);
+        Console.WriteLine("KONIEC GRY");
+        Console.WriteLine(info);
+        Console.WriteLine("Naciśnij ENTER aby wyjść...");
+        Console.ReadLine();
         Environment.Exit(0);
     }
 }
